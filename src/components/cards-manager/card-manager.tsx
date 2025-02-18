b03c9711201196daa717beba0deebf1d3f9d2d34
AN-UNKNOWN <anuragpindoriya@gmail.com>
2025-02-19 01:40:01 +0530

    update card-manager component to enhance responsive margins

--- src/components/cards-manager/card-manager.tsx
+++ src/components/cards-manager/card-manager.tsx
@@ -10,7 +10,7 @@ export default function CardManager() {
     setActiveTab(index)
   }
   return (
-    <div className={'bg-white mx-[34px]'}>
+    <div className={'bg-white md:mx-[34px] sm:mx-[20px] mx-[10px]'}>
       <div className={'w-full bg-white px-6 pt-[10px] mt-[24px] flex justify-between font-montserrat'}
            style={{ boxShadow: '0px 1px 2px 0px #6A94A5' }}>
         <div className={'flex gap-4 self-end'}>
