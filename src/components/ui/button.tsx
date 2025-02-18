6e1facd2761e0c3f9dc2f44cadcd5a7a3712071c
AN-UNKNOWN <anuragpindoriya@gmail.com>
2025-02-18 10:31:31 +0530

    change default size in button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         'pd-default': 'bg-[#0FA1DB] text-white shadow-sm hover:bg-[#0FA1DB]/90 rounded-[2px] hover:bg-[#0FA1DB]/80 px-[10px] py-[3px] gap-[4px] text-[12px]',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
+        default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
