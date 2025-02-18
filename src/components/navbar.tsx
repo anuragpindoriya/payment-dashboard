dd5965365eebd339ef172f9fbb0a9b326c066088
AN-UNKNOWN <anuragpindoriya@gmail.com>
2025-02-19 00:46:45 +0530

    update Navbar and add Sidebar components to enhance navigation functionality and layout

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,11 +3,17 @@ import { AlignJustify } from 'lucide-react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar.tsx'
 
 
-export default function Navbar() {
+interface NavbarProps {
+  isSidebarOpen: boolean;
+  setIsSidebarOpen: (isOpen: boolean) => void;
+}
+
+export default function Navbar({ isSidebarOpen, setIsSidebarOpen }: NavbarProps) {
   return <>
     <div className={'w-full flex items-center justify-between px-6 py-4'}>
       <div className={'flex items-center gap-4'}>
-        <Button variant="ghost"><AlignJustify /></Button>
+        <Button variant="ghost" className="cursor-pointer"
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}><AlignJustify /></Button>
         <span>Cards</span>
       </div>
       <div>
