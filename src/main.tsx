2be4ac905ad971b4624b4fa3dede73dbc34ce7ae
AN-UNKNOWN <anuragpindoriya@gmail.com>
2025-02-18 23:01:51 +0530

    add Toaster component for improved notification handling

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import App from './App.tsx'
 
 import { Provider } from 'react-redux'
 import { store } from '@/store.ts'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </StrictMode>,
 )
